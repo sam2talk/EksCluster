d647b3086ebeef48875cb438f4268f7baaa6443b
sam2talk <samstation09@gmail.com>
2023-04-08 18:55:39 +0000

    variabless

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "subnet_id_1" {
-  type = string
+  type    = string
   default = "subnet-ec90408a"
- }
+}
 
- variable "subnet_id_2" {
-  type = string
+variable "subnet_id_2" {
+  type    = string
   default = "subnet-0a911b04"
- }
+}
 
- variable "cluster_name" {
-  type = string
+variable "cluster_name" {
+  type    = string
   default = "my-eks-cluster"
-  }
+}
