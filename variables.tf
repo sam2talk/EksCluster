0c2ce72a70ef777c696f023371b4c3d4f5af357f
Samuel A <105013101+sam2talk@users.noreply.github.com>
2023-04-08 14:49:35 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "subnet_id_1" {
   type = string
-  default = "subnet-ec90408a"
+  default = "subnet-043199e3fcc077f76"
  }
 
  variable "subnet_id_2" {
   type = string
-  default = "subnet-0a911b04"
+  default = "subnet-0ef2a0bbdb7ee6aa5"
  }
 
  variable "cluster_name" {
